9befc5c3fb7567c573c2f421192dc4850bd59bd3
Mathieu <petitjean.ma@gmail.com>
2017-05-28 17:55:30 +0200

    Small fix + added the idea of the pattern, WIP

--- Code/main.m
+++ Code/main.m
@@ -18,6 +18,7 @@ mgc = computeMGC(puzzle);
 ccc = mgc .* ssd.^(1/q);
 
 endpuzzle = zeros(size(puzzle));
+pattern = zeros(m/blk_size,n/blk_size);
 for ii = 1:4
     ssd(ii,ii,:) = Inf;
 end
@@ -25,7 +26,9 @@ startpiece = randi([1 size(puzzle,3)]);
 endimage = zeros(m,n);
 midposrow = round(m/blk_size/2-1)*blk_size;
 midposcol = round(n/blk_size/2-1)*blk_size;
-endimage(midposrow+1:midposrow+blk_size, midposcol+1:midposcol+blk_size) = puzzle(:,:,startpiece);
+endimage(midposrow+1:midposrow+blk_size, midposcol+1:midposcol+blk_size) = ...
+    puzzle(:,:,startpiece);
+pattern(round(m/blk_size/2),round(n/blk_size/2)) = 1;
 
 figure;
 imshow(endimage)
@@ -35,8 +38,11 @@ startposcol = midposcol+1;
 
 for ii=1:size(puzzle,3)-1
    [mpiece, loc] = findbestmatch(startpiece,ssd);
-   disp(loc)
    pause;
+   
+   % TODO
+   % Check pattern, redéfinir un point de départ si entouré de 1   
+   
    switch loc
        case 2
            % Piece should be placed on the left
@@ -44,50 +50,63 @@ for ii=1:size(puzzle,3)-1
                % Shift puzzle to the right if needed
                endimage = circshift(endimage,blk_size,2);
                startposcol = startposcol + blk_size;
+               pattern = circshift(pattern,1,2);
            end
+           
            endimage(startposrow:startposrow+blk_size-1,startposcol-blk_size:startposcol-1)...
                = puzzle(:,:,mpiece);
            startposcol = startposcol-blk_size;
+          
        case 1
            % Piece should be placed on the right
            if startposcol == n-blk_size+1
                % Shift puzzle to the left if needed
                endimage = circshift(endimage,-blk_size,2);
                startposcol = startposcol - blk_size;
+               pattern = circshift(pattern,-1,2);
            end
+           
            endimage(startposrow:startposrow+blk_size-1,startposcol+blk_size:startposcol+2*blk_size-1)...
                = puzzle(:,:,mpiece);
            startposcol = startposcol+blk_size;
+           
        case 4
             % Piece should be placed on the top
             if startposrow == 1
                % Shift puzzle to the bottom if needed
                endimage = circshift(endimage,blk_size,1);
                startposrow = startposrow + blk_size;
-           end
+               pattern = circshift(pattern,1,1);
+            end
+           
            endimage(startposrow-blk_size:startposrow-1,startposcol:startposcol+blk_size-1)...
                = puzzle(:,:,mpiece);
            startposrow = startposrow-blk_size;
+           
        case 3
            % Piece should be placed on the bottom
             if startposrow == m-blk_size+1
                % Shift puzzle to the top if needed
                endimage = circshift(endimage,-blk_size,1);
                startposrow = startposrow - blk_size;
-           end
+               pattern = circshift(pattern,-1,1);
+            end
+           
            endimage(startposrow+blk_size:startposrow+2*blk_size-1,startposcol:startposcol+blk_size-1)...
                = puzzle(:,:,mpiece);
            startposrow = startposrow + blk_size;
-
    end
+   % Update pattern
    
+   pattern(round((startposrow-1)/blk_size)+1,round((startposcol-1)/blk_size+1)) = 1;
+   caca = 1;
    % Put processed image SSD value to Inf
-   ssd(startpiece,mpiece,:) = Inf;
-   ssd(mpiece,startpiece,:) = Inf;
+   ssd(startpiece,:,:) = Inf;
+   ssd(:,startpiece,:) = Inf;
    
    % Should systematically put to Inf blocs next to each other
    
-   
+   disp(pattern)
    imshow(endimage)
    startpiece = mpiece;
 end
